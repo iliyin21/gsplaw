784c109bc76ec79a76f9ec78faaa4866ff325096
Ganang <ahmadiliyin78@gmail.com>
2026-08-04 15:16:40 +0700

    fix auto migrate kolom gallery

---
+++ git add db.js
@@ -0,0 +1,8 @@
+[33mcommit e615dd785a2889ef40f60a2d373cf38d52638fc6[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m
+Author: Ganang <ahmadiliyin78@gmail.com>
+Date:   Tue Aug 4 14:59:28 2026 +0700
+
+    fix:auto-migrate kolom gallery yang hilang agar upload foto/video/youTube tidak eror
+
+ db.js | 30 [32m++++++++++++++++++++++++++++++[m
+ 1 file changed, 30 insertions(+)
